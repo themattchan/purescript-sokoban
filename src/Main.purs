1883a1c7cd53685446892f500a888f5ef1aabd4b
Matthew Chan <matt@themattchan.com>
2016-11-21 15:42:32 -0800

    foo

--- src/Main.purs
+++ src/Main.purs
@@ -3,19 +3,12 @@ module Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import
-data Board = Board
-  { width  :: Int
-  , height :: Int
-  , board  :: Array (Array Space)
-  }
 
-data Space
-  = Wall
-  | Box
-  | Goal Bool
-  | Empty
-  deriving (Show, Eq)
+import Pux (renderToDOM, fromSimple, start)
+import Pux.Html (Html, text, button, span, div)
+import Pux.Html.Events (onClick)
+
+import Types
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
